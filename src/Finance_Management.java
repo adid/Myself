d661b32d6995c5b1736a9b2667e3e7a59fc515a5
Adid Al Mahamud Shazid <adid.shazid@gmail.com>
2023-10-17 20:57:55 +0600

    Create user updated

--- src/Finance_Management.java
+++ src/Finance_Management.java
@@ -1,32 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-class Transaction
-{
-    private String description;
-    private double amount;
 
-    public Transaction(String description, double amount)
-    {
-        this.description = description;
-        this.amount = amount;
-    }
-
-    public String getDescription()
-    {
-        return description;
-    }
-
-    public double getAmount()
-    {
-        return amount;
-    }
-
-    @Override
-    public String toString()
-    {
-        return description + ": $" + amount;
-    }
-}
 
 public class Finance_Management
 {
@@ -71,7 +45,7 @@ public class Finance_Management
     public void run() {
         boolean running = true;
         while (running) {
-            System.out.println("Manage you finance with Myself!");
+            System.out.println("Manage you finance with Myself");
             System.out.println("1. Add Transaction");
             System.out.println("2. View Transactions");
             System.out.println("3. Quit");
